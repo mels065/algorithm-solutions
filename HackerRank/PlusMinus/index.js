7f6ae3d8f6e2d0d25169d8cef056c2f34a667508
Brandon Mellus <mellusbrandon@gmail.com>
2022-10-20 18:45:13 -0400

    [MOD] Add 'Plus Minus' problem from HackerRank

---
+++ HackerRank/PlusMinus/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let positives = 0;
+    let negatives = 0;
+    let zeros = 0;
+    arr.forEach(num => {
+        if (num > 0) {
+            positives++;
+        } else if (num < 0) {
+            negatives++;
+        } else {
+            zeros++;
+        }
+    });
+    
+    console.log((positives / arr.length).toFixed(6));
+    console.log((negatives / arr.length).toFixed(6));
+    console.log((zeros / arr.length).toFixed(6));
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
