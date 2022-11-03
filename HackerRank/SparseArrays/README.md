f4ac117b9fd5c1c485013cd85a27498ad48c0a34
Brandon Mellus <mellusbrandon@gmail.com>
2022-11-02 20:02:55 -0400

    [MOD] Add solution to Sparse Arrays algorithm problem

---
+++ HackerRank/SparseArrays/README.md
@@ -0,0 +1,3 @@
+# Sparse Arrays
+
+The problem can be found [here](https://www.hackerrank.com/challenges/three-month-preparation-kit-sparse-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one)
---
+++ HackerRank/SparseArrays/main.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    str_dict = {}
+    for string in strings:
+        if string not in str_dict:
+            str_dict[string] = 0
+        str_dict[string] += 1
+    
+    result = []
+    for query in queries:
+        if query not in str_dict:
+            result.append(0)
+        else:
+            result.append(str_dict[query])
+    
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
