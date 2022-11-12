7dd96d89243c41d96ed7c4e00acd7aa3fd137740
Brandon Mellus <mellusbrandon@gmail.com>
2022-11-12 11:40:20 -0500

    [MOD] Add solution to Grading Students problem

---
+++ HackerRank/GradingStudents/main.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    rounded_grades = []
+    for grade in grades:
+        multiple = 40
+        while multiple < grade: multiple += 5
+        if grade >= 38 and multiple - grade < 3:
+            rounded_grades.append(multiple)
+        else:
+            rounded_grades.append(grade)
+    
+    return rounded_grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
