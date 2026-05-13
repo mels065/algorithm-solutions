022d492f930ebf66f586465e5336ecf882d87826
Brandon Mellus <mellusbrandon@gmail.com>
2026-05-12 23:06:10 -0400

    [CodeWars] -> Texas Hold 'Em Hands (In-Progress)

--- CodeWars/2026/05_May/TexasHoldEmHands/Kata.java
+++ CodeWars/2026/05_May/TexasHoldEmHands/Kata.java
@@ -7,28 +7,41 @@ public record Hand(String type, String[] ranks) {}
 import java.util.HashMap;
 import org.apache.commons.lang3.ArrayUtils;
 
-record SuitCount(char suit, int count) {}
+record SuitCount(char suit, int count, int strength) {
+  SuitCount(char suit) {
+    this(suit, 0, 0);
+  }
+}
+record FlushData(boolean isFlush, SuitCount suitCount) {}
 
 public class Kata {
     public static Hand hand(String[] holeCards, String[] communityCards) {
         String[] cards = ArrayUtils.addAll(holeCards, communityCards);
+      
         System.out.println(highestSuitCount(cards));
         return new Hand("nothing", new String[] { "A", "Q", "9", "6", "3" });
     }
   
-    private static SuitCount highestSuitCount(String[] cards) {
-      HashMap<String, Integer> suitCountMap = new HashMap<String, Integer>();
-        suitCountMap.put("♣", 0);
-        suitCountMap.put("♥", 0);
-        suitCountMap.put("♠", 0);
-        suitCountMap.put("♦", 0);
-
-      System.out.println(suitCountMap);
+    private static FlushData highestSuitCount(String[] cards) {
+      HashMap<String, SuitCount> suitCountMap = new HashMap<String, SuitCount>();
+        suitCountMap.put("♣", new SuitCount('♣'));
+        suitCountMap.put("♥", new SuitCount('♥'));
+        suitCountMap.put("♠", new SuitCount('♠'));
+        suitCountMap.put("♦", new SuitCount('♦'));
       
       for (String card : cards) {
-        char suit = card.charAt(1);
+        String suit = card.substring(1, 2);
+        String rank = card.substring(0, 1);
       }
       
-      return new SuitCount("♣".charAt(0), 0);
+      return new FlushData(true, suitCountMap.get("♣"));
+    }
+  
+    private static int convertRankToInt(String rank) {
+      return switch(rank) {
+          case "A" -> 1;
+          case "J", "Q", "K" -> 11;
+          default -> Integer.parseInt(rank);
+      };
     }
 }
