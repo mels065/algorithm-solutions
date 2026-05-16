19c58fbf34b75d93aa1c713292ff10ed5e572f21
Brandon Mellus <mellusbrandon@gmail.com>
2026-05-16 18:49:29 -0400

    [CodeWars] Texas Hold 'Em Hands -> Remove unused import

--- CodeWars/2026/05_May/TexasHoldEmHands/Kata.java
+++ CodeWars/2026/05_May/TexasHoldEmHands/Kata.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.regex.*;
 import org.apache.commons.lang3.ArrayUtils;
 
