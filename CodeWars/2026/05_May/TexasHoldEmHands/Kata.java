006788b42775fa5f41dd1faecb9b7e1b001e5be5
Brandon Mellus <mellusbrandon@gmail.com>
2026-05-16 18:47:47 -0400

    [CodeWars] Texas Hold 'Em Hands (In-Progress

--- CodeWars/2026/05_May/TexasHoldEmHands/Kata.java
+++ CodeWars/2026/05_May/TexasHoldEmHands/Kata.java
@@ -4,79 +4,157 @@
 public record Hand(String type, String[] ranks) {}
 */
 
+import java.lang.IllegalArgumentException;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.regex.*;
 import org.apache.commons.lang3.ArrayUtils;
 
-class FlushData {
-  public char suit;
-  public int count, strength;
-  public String[] cards;
+class Card implements Comparable<Card> {
+  final Pattern cardPattern = Pattern.compile("(10|[0-9AJQK])([♣♥♠♦])");
   
-  FlushData(char s, int c, int st, String[] crds) {
+  private char suit;
+  private String rank;
+  
+  public Card(String card) throws IllegalArgumentException {
+    Matcher cardMatcher = cardPattern.matcher(card);
+    
+    if (!cardMatcher.find()) {
+      throw new IllegalArgumentException("Card string must be the format of rank and then suit (e.g. K♣, 10♦, etc.)");
+    }
+    
+    rank = cardMatcher.group(1);
+    suit = cardMatcher.group(2).charAt(0);
+  }
+  
+  public char getSuit() {
+    return suit;
+  }
+  
+  public String getRank() {
+    return rank;
+  }
+  
+  public int getRankAsInt() {
+    return switch(rank) {
+        case "A" -> 1;
+        case "J" -> 11;
+        case "Q" -> 12;
+        case "K" -> 13;
+        default -> Integer.parseInt(rank);
+    };
+  }
+  
+  @Override
+  public String toString() {
+    return String.valueOf(rank) + String.valueOf(suit);
+  }
+  
+  @Override
+  public int compareTo(Card otherCard) {
+    return Integer.compare(getRankAsInt(), otherCard.getRankAsInt());
+  }
+  
+  public static Card[] convertStringsToCards(String[] cardStrings) {
+    Card[] newCards = new Card[]{};
+    for (String cardStr: cardStrings) {
+      newCards = ArrayUtils.add(newCards, new Card(cardStr));
+    }
+    
+    return newCards;
+  }
+  
+  public static int compare(Card card1, Card card2) {
+    return card1.compareTo(card2);
+  }
+}
+
+class FlushData implements Comparable<FlushData> {
+  private char suit;
+  public Card[] cards;
+  
+  public FlushData(char s, Card[] crds) {
     suit = s;
-    count = c;
-    strength = st;
     cards = crds;
   }
   
-  FlushData(char s) {
-    this(s, 0, 0, new String[]{});
+  public FlushData(char s) {
+    this(s, new Card[]{});
+  }
+  
+  public char getSuit() {
+    return suit;
+  }
+  
+  public void addCard(Card card) {
+    cards = ArrayUtils.add(cards, card);
+  }
+  
+  @Override
+  public int compareTo(FlushData otherFlushData) {
+    return Card.compare(
+      getStrongestCard(),
+      otherFlushData.getStrongestCard()
+    );
+  }
+  
+  private Card getStrongestCard() throws NoSuchElementException {
+    return Arrays.stream(cards)
+      .max(Card::compare).get();
+  }
+  
+  public static int compare(FlushData fd1, FlushData fd2) {
+    return fd1.compareTo(fd2);
+  }
+}
+
+class FlushDataMap {
+  private HashMap<String, FlushData> flushDataMap;
+  
+  public FlushDataMap() {
+    flushDataMap = new HashMap<String, FlushData>();
+    flushDataMap.put("♣", new FlushData('♣'));
+    flushDataMap.put("♥", new FlushData('♥'));
+    flushDataMap.put("♠", new FlushData('♠'));
+    flushDataMap.put("♦", new FlushData('♦'));
+  }
+  
+  public void addCards(Card[] cards) {
+    for (Card crd: cards) {
+      addCard(crd);
+    }
+  }
+  
+  public boolean hasFlush() {
+    return flushDataMap
+      .values()
+      .stream()
+      .anyMatch(fd -> fd.cards.length >= 5);
+  }
+  
+  public FlushData getStrongestSuit() throws NoSuchElementException {
+    return flushDataMap
+      .values()
+      .stream()
+      .max(FlushData::compare).get();
+  }
+  
+  private void addCard(Card card) {
+    flushDataMap.get(String.valueOf(card.getSuit())).addCard(card);
   }
 }
 
 public class Kata {
     public static Hand hand(String[] holeCards, String[] communityCards) {
-        String[] cards = ArrayUtils.addAll(holeCards, communityCards);
-        Arrays.sort(cards, Comparator.comparing(Kata::getRank));
-        HashMap<String, FlushData> flushMap = generateFlushMap(cards);
-      
-        for (FlushData fd: flushMap.values()) {
-          System.out.println(fd.suit);
-          System.out.println(fd.count);
-          System.out.println(fd.strength);
-
-          for (String card: fd.cards) {
-            System.out.println(card);
-          }
-        }
+        Card[] cards = Card.convertStringsToCards(ArrayUtils.addAll(holeCards, communityCards));
+        
+        Arrays.sort(cards);
+        FlushDataMap flushDataMap = new FlushDataMap();
+        flushDataMap.addCards(cards);
 
         return new Hand("nothing", new String[] { "A", "Q", "9", "6", "3" });
-    }
-  
-    private static HashMap<String, FlushData> generateFlushMap(String[] cards) {
-      HashMap<String, FlushData> flushMap = new HashMap<String, FlushData>();
-        flushMap.put("♣", new FlushData('♣'));
-        flushMap.put("♥", new FlushData('♥'));
-        flushMap.put("♠", new FlushData('♠'));
-        flushMap.put("♦", new FlushData('♦'));
-      
-      for (String card : cards) {
-        String suit = getSuit(card);
-        int rank = getRank(card);
-        
-        FlushData fd = flushMap.get(suit);
-        fd.count += 1;
-        fd.strength += rank;
-        ArrayUtils.add(fd.cards, card);
-      }
-      
-      return flushMap;
-    }
-  
-    private static String getSuit(String card) {
-      return card.substring(1, 2);
-    }
-  
-    private static int getRank(String card) {
-      String rank = card.substring(0, 1);
-      return switch(rank) {
-          case "A" -> 1;
-          case "J" -> 11;
-          case "Q" -> 12;
-          case "K" -> 13;
-          default -> Integer.parseInt(rank);
-      };
     }
 }
